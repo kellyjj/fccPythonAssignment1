0e834b206be5ebae81f50c1cee2dd31f5f95df08
kelly j <kellyjj@yahoo.com>
2020-08-03 10:50:18 -0700

    1st committ

---
+++ assignment1_kjj.py
@@ -0,0 +1,11 @@
+#kelly j.  8-3-20  1st assignment for python class at fresno city instructor rio waller.
+#this program will say hello and prompt for my name
+
+print('Hello World')
+print('What is your Name: ')  #ask for users name
+myname = input()
+print('it is nice to meet you '+myname)
+print('the length of your name is '+str(len(myname)))
+print('What is your age ') #get age
+myage = input()
+print('you will be '+str(int(myage)+1)+' in a year ')
